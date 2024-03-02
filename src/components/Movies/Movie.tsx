64dff0fb8b2f74f6f778b1e405db8bcdf2d281f5
Alifoxy <nightfoxy666@gmail.com>
2024-03-03 01:16:28 +0300

    commit 1

--- src/components/Movies/Movie.tsx
+++ src/components/Movies/Movie.tsx
@@ -1,8 +1,7 @@
-import {FC, PropsWithChildren} from "react";
+import React, {FC, PropsWithChildren} from "react";
 import {useNavigate} from "react-router-dom";
 import {IMovie} from "../../interfaces";
-import {StarRatings} from "react-star-ratings";
-import {render} from "@testing-library/react";
+
 
 
 interface IProps extends PropsWithChildren {
@@ -11,26 +10,16 @@ interface IProps extends PropsWithChildren {
 }
 const Movie: FC<IProps> = ({Movie,page}) => {
     const {title, vote_average, poster_path} = Movie;
-    const starRatings = (name) => {
-        render() {
-            // rating = 2;
-            return (
-                <StarRatings
-                    rating={this.state.rating}
-                    starRatedColor="blue"
-                    changeRating={this.changeRating}
-                    numberOfStars={6}
-                    name='rating'
-                />
-            );
-        }
-    };
+
+
+
     const toDetails = () => {
         const mov_id = Movie.id;
         navigate(`${page}/details/${mov_id}`)
     };
 
     const navigate = useNavigate()
+    // @ts-ignore
     return (
         <div className={'mini_block'}>
             <div className={'inner_movie_block'}>
@@ -38,15 +27,8 @@ const Movie: FC<IProps> = ({Movie,page}) => {
                 <div>
                     <img  className={'main_poster'} src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={'poster'}/>
                 </div>
-                <StarRatings
-                    rating={vote_average}
-                    starRatedColor="orangered"
-                    numberOfStars={10}
-                    name='rating'
-                />
-
-                <div>vote: {vote_average}</div>
-                <div>
+                <div className={'vote'}>vote: {vote_average}</div>
+                <div >
                     <button onClick = {toDetails} className={'det_button'}>Details... </button>
                 </div>
             </div>
